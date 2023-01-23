a88268b816f812116535a3e48b09325b3951354e
Lingo <84602494+cyld101@users.noreply.github.com>
2023-01-23 17:44:14 +0000

    fromCounterToScoreboard

---
+++ index.js
@@ -0,0 +1,32 @@
+let hmScoreEl = document.getElementById("hmscore-el")
+let count1 = 0
+
+function add1h() {
+    count1 += 1
+    hmScoreEl.textContent = count1
+}
+function add2h() {
+    count1 += 2
+    hmScoreEl.textContent = count1
+}
+function add3h() {
+    count1 += 3
+    hmScoreEl.textContent = count1
+}
+
+
+let guScoreEl = document.getElementById("guscore-el")
+let count2 = 0
+
+function add1g() {
+    count2 += 1
+    guScoreEl.textContent = count2
+}
+function add2g() {
+    count2 += 2
+    guScoreEl.textContent = count2
+}
+function add3g() {
+    count2 += 3
+    guScoreEl.textContent = count2
+}
